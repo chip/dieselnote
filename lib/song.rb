898be7557c8d065dfeea5996bc40d45f1d1643d2
CulleyS <culley.smith@gmail.com>
2013-01-23 08:50:26 -0600

    remove #album from Song class

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,19 @@
+# Song Class
+#
+# A song requires a title & artist
+# However, it can build dynamic attributes such as
+# Genre, Date, Album, or any the user wishes to create
+# on the fly
+#
 class Song
   attr_reader :title
-  attr_accessor :artist, :album
+  attr_accessor :artist
 
   def initialize(title)
     @title = title
   end
 
   def to_s
-    "#{title} - #{artist} :: #{album}"
+    "#{title} - #{artist}"
   end
 end
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -6,17 +6,15 @@ describe Song do
 
   before do
     song.artist = 'The Beatles'
-    song.album = 'The Beatles (The White Album)'
   end
 
   subject { song }
 
   its(:title) { should eq 'Dear Prudence' }
   its(:artist) { should eq 'The Beatles' }
-  its(:album) { should eq 'The Beatles (The White Album)' }
 
   it 'has a custom #to_s method' do
     expect(song.to_s).
-      to eq 'Dear Prudence - The Beatles :: The Beatles (The White Album)'
+      to eq 'Dear Prudence - The Beatles'
   end
 end
